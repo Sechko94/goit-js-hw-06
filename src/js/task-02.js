6e80536e362a98045bb16aed4a12b5be2fbb6978
Sergiy Sechko <ssechko94@gmail.com>
2023-03-18 16:24:14 +0200

    Done task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("#ingredients")
+
+
+const elements = ingredients.map(ingredient => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = ingredient;
+  itemEl.style.color = "tomato";
+  return itemEl;
+})
+
+ingredientsList.append(...elements);
+
+console.log(ingredientsList);
+
