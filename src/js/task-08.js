0fc978131e32566a47bd5e39f1c8f4de04003f9a
Sergiy Sechko <ssechko94@gmail.com>
2023-03-24 21:51:03 +0200

    add task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,28 @@
 
+// Отримання елементів форми
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', (event) => {
+    // Зупиняємо перезавантаження сторінки
+    event.preventDefault(); 
+
+//  отримую посилання на інпути електронної пошти та пароля
+    const inputEmail = loginForm.elements.email;
+    const inputPassword = loginForm.elements.password;
+
+    // Перевірка на наявність порожніх полів
+    if (inputEmail.value === '' || inputPassword.value === '') {
+        alert("Будь ласка, заповніть всі поля");
+    } else {
+        const formData = {
+            email: inputEmail.value,
+            password: inputPassword.value
+        };
+        console.log(formData);
+        loginForm.reset();
+    } 
+    
+})
+
+
+
